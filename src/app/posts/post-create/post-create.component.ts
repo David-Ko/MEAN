c3f2e8e8c04b2688024d0fd3e3d0fe3fe0dea0b8
David Ko <dko@davids-imac.lan>
2019-07-09 14:23:01 -0700

    Added route and edit features

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, EventEmitter, Output } from "@angular/core";
 import { Post } from "../../entities/post.model";
 import { NgForm } from "@angular/forms";
 import { PostsService } from "src/app/service/posts.service";
+import { ActivatedRoute, ParamMap } from "@angular/router";
 
 @Component({
   selector: "app-post-create",
@@ -11,17 +12,48 @@ import { PostsService } from "src/app/service/posts.service";
 export class PostCreateComponent implements OnInit {
   enteredTitle = "";
   enteredContent = "";
+  private mode = "create;";
+  private postId: string;
+  post: Post;
   // @Output() postCreated = new EventEmitter<Post>();
-  constructor(public postsService: PostsService) {}
+  constructor(
+    public postsService: PostsService,
+    public route: ActivatedRoute
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has("postId")) {
+        this.mode = "edit";
+        this.postId = paramMap.get("postId");
+        this.postsService.getPost(this.postId).subscribe(postData => {
+          this.post = {
+            id: postData._id,
+            title: postData.title,
+            content: postData.content
+          };
+        });
+      } else {
+        this.mode = "create";
+        this.postId = null;
+      }
+    });
+  }
 
   onAddPost(form: NgForm) {
     if (form.invalid) {
       return;
     }
-    this.postsService.addPost(form.value.title, form.value.content);
-    form.resetForm();
+    if (this.mode === "create") {
+      this.postsService.addPost(form.value.title, form.value.content);
+      form.resetForm();
+    } else {
+      this.postsService.updatePost(
+        this.postId,
+        form.value.title,
+        form.value.content
+      );
+    }
     // const post: Post = {
     //   title: form.value.title,
     //   content: form.value.content
